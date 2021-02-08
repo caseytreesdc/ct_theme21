dfc4114b7a67408194c144d20d6f73b34394796b
TKhosla <tkhosla@caseytrees.org>
2021-02-08 16:51:45 -0500

    Plant__page-5

--- page-plant.php
+++ page-plant.php
@@ -36,5 +36,144 @@
             </p>
         </section>
     </div>
+    <div class="Plant__page-2">
+        <div class="Plant__page-2-text-and-image">
+            <div class="Plant__page-2-text">
+                <h2 class="Plant__page-2-text-h2">Home</h2>
+                <p class="Plant__page-2-text-p">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+                    magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                    quis nostrud exerci tation ullamcorper suscipit lobortis
+                    nisl ut aliquip ex ea commodo consequat. Duis autem ssim
+                    qui blandit praesent luptatum zzril delenit augue duis dolore
+                    te feugait nulla facilisi.Lorem ipsum dolor sit amet.
+                </p>
+                <a href="" class="Plant__page-2-text-a">
+                    Not sure which program is best for you? Take this quiz!
+                </a>
+            </div>
+            <div 
+                class="Plant__page-2-image"
+                style="
+                    background: linear-gradient(
+                        rgba(55, 104, 178, 0.5),
+                        rgba(55, 104, 178, 0.5)
+                    ),
+                    url(https://caseytrees.org/wp-content/uploads/2021/01/small-shade-email.png);
+                    background-position: center;
+                    background-size: cover;
+                    background-repeat: no-repeat;">
+            </div>
+        </div>
+        <div class="Plant__page-2-links">
+            <a class="Plant__page-2-links-a" href="">FFS</a>
+            <a class="Plant__page-2-links-a" href="">RSH</a>
+            <a class="Plant__page-2-links-a" href="">REBATE</a>
+            <a class="Plant__page-2-links-a" href="">PEPCO</a>
+        </div>
+    </div>
+    <div class="Plant__page-3">
+        <div class="Plant__page-3-text-and-image">
+            <div 
+                class="Plant__page-3-image"
+                style="
+                    background: linear-gradient(
+                        rgba(123, 158, 129, 0.5),
+                        rgba(123, 158, 129, 0.5)
+                    ),
+                    url(https://caseytrees.org/wp-content/uploads/2021/02/tubman-email.png);
+                    background-position: center;
+                    background-size: cover;
+                    background-repeat: no-repeat;">
+            </div>
+            <div class="Plant__page-3-text">
+                <h2 class="Plant__page-3-text-h2">School</h2>
+                <p class="Plant__page-3-text-p">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+                    magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                    quis nostrud exerci tation ullamcorper suscipit lobortis
+                    nisl ut aliquip ex ea commodo consequat. Duis autem ssim
+                    qui blandit praesent luptatum zzril delenit augue duis dolore
+                    te feugait nulla facilisi.Lorem ipsum dolor sit amet.
+                </p>
+                <p class="Plant__page-3-text-p">
+                    <a href="" class="Plant__page-3-text-p-a">
+                        APPLY NOW
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="Plant__page-4">
+        <div class="Plant__page-4-text-and-image">
+            <div class="Plant__page-4-text">
+                <h2 class="Plant__page-4-text-h2">Community</h2>
+                <p class="Plant__page-4-text-p">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+                    magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                    quis nostrud exerci tation ullamcorper suscipit lobortis
+                    nisl ut aliquip ex ea commodo consequat. Duis autem ssim
+                    qui blandit praesent luptatum zzril delenit augue duis dolore
+                    te feugait nulla facilisi.Lorem ipsum dolor sit amet.
+                </p>
+                <a href="/calendar/" class="Plant__page-4-text-a">
+                    Calendar of Upcoming Events
+                </a>
+            </div>
+            <div 
+                class="Plant__page-2-image"
+                style="
+                    background: linear-gradient(
+                        rgba(225, 131, 35, 0.5),
+                        rgba(225, 131, 35, 0.5)
+                    ),
+                    url(https://caseytrees.org/wp-content/uploads/2020/12/eoy-thanks-email.png);
+                    background-position: center;
+                    background-size: cover;
+                    background-repeat: no-repeat;">
+            </div>
+        </div>
+        <div class="Plant__page-4-links">
+            <a class="Plant__page-4-links-a" href="">APPLY NOW</a>
+            <a class="Plant__page-4-links-a" href="">GROUP VOLUNTEERS</a>
+            <a class="Plant__page-4-links-a" href="">COMMUNITY PARTNERSHIPS</a>
+        </div>
+    </div>
+    <div class="Plant__page-5">
+        <div class="Plant__page-5-text-and-image">
+            <div 
+                class="Plant__page-5-image"
+                style="
+                    background: linear-gradient(
+                        rgba(164, 83, 46, 0.5),
+                        rgba(164, 83, 46, 0.5)
+                    ),
+                    url(https://caseytrees.org/wp-content/uploads/2020/10/nuttall-email.png);
+                    background-position: center;
+                    background-size: cover;
+                    background-repeat: no-repeat;"">
+            </div>
+            <div class="Plant__page-5-text">
+                <h2 class="Plant__page-5-text-h2">Dedication</h2>
+                <p class="Plant__page-5-text-p">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+                    magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                    quis nostrud exerci tation ullamcorper suscipit lobortis
+                    nisl ut aliquip ex ea commodo consequat. Duis autem ssim
+                    qui blandit praesent luptatum zzril delenit augue duis dolore
+                    te feugait nulla facilisi.Lorem ipsum dolor sit amet.
+                </p>
+                <p class="Plant__page-5-text-p">
+                    <a href="" class="Plant__page-5-text-p-a">
+                        DEDICATE NOW
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 <?php get_footer(); ?>
