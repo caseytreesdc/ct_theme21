77c188bf4dda95c393cdc4fb1b129286a2365cde
TKhosla <tkhosla@caseytrees.org>
2021-02-08 14:50:40 -0500

    Plant__page-1

--- page-plant.php
+++ page-plant.php
@@ -1,41 +1,40 @@
+<!-- printf( __( 'The post type is: %s', 'textdomain' ), get_post_type( get_the_ID() ) ); -->
 <?php get_header(); ?>
-<div class="Breadcrumbs">
-  <a class="Breadcrumbs__anchor" href="https://www.google.com/"
-    ><p class="Breadcrumbs__text"><?php ct2021_get_breadcrumbs(); ?></p></a
-  >
+<div class="Plant">
+    <div class="Plant__page-1">
+        <section class=Plant__page-1-section-1>
+            <h1 class="Plant__page-1-section-1-h1">
+                There are so many ways to plant with us. Where do you want to see more trees?
+            </h1>
+        </section>
+        <section class="Plant__page-1-section-2-icons">
+            <div class="Plant__page-1-section-2-icon-w-text">
+                <?php get_template_part('svgs/inline', 'home') ?>
+                <p class="Plant__page-1-section-2-p">HOME</p>
+            </div>
+            <div class="Plant__page-1-section-2-icon-w-text">
+                <?php get_template_part('svgs/inline', 'school') ?>
+                <p class="Plant__page-1-section-2-p">SCHOOL</p>
+            </div>
+            <div class="Plant__page-1-section-2-icon-w-text">
+                <?php get_template_part('svgs/inline', 'community') ?>
+                <p class="Plant__page-1-section-2-p">COMMUNITY</p>
+            </div>
+            <div class="Plant__page-1-section-2-icon-w-text">
+            <?php get_template_part('svgs/inline', 'dedication') ?>
+                <p class="Plant__page-1-section-2-p">DEDICATION</p>
+            </div>
+        </section>
+        <section class="Plant__page-1-section-3">
+            <p class="Plant__page-1-section-3-p">
+                Have something else in mind? 
+                <a 
+                    class="Plant__page-1-section-3-a"
+                    href="mailto:friends@caseytrees.org?subject=Planting">
+                        Let us know.
+                </a>
+            </p>
+        </section>
+    </div>
 </div>
-<?php 
-    if( have_posts() ) {
-        while( have_posts() ) {
-            the_post();
-            printf( __( 'The post type is: %s', 'textdomain' ), get_post_type( get_the_ID() ) );
-            ?><h2>
-                <a href="<?php the_permalink();?>"><?php the_title();?></a>
-            </h2>
-            <p><?php echo get_the_date(); ?> by <?php echo get_field('author');?> </p>       
-            <?php 
-                $image = get_field('image');
-                $size = 'full'; // (thumbnail, medium, large, full or custom size)
-                if( $image ) {
-                    echo wp_get_attachment_image( $image, $size );
-                }
-            ?>
-            <?php echo get_the_category_list(); ?>
-            <?php the_content();?>
-            <?php 
-
-            $fields = get_fields();
-            
-            if( $fields ): ?>
-                <ul>
-                    <?php foreach( $fields as $name => $value ): ?>
-                        <li><b><?php echo $name; ?></b> <?php echo $value; ?></li>
-                    <?php endforeach; ?>
-                </ul>
-
-            <?php endif; ?>
-            <?php
-        }
-    }
-?>
 <?php get_footer(); ?>
