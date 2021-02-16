72262ea344b7f14754c577d118cf3cccb7391f41
TKhosla <tkhosla@caseytrees.org>
2021-02-16 16:13:32 -0500

    first beta testing

--- home.php
+++ home.php
@@ -1,38 +1,48 @@
-<?php get_header(); ?>
-<div class="Leaflet__home">
-    <div class="Leaflet__posts-list">
-        <?php 
-            if( have_posts() ) {
-                while( have_posts() ) {
-                    the_post();?>
-                    <div class="Leaflet__post">
-                        <h3>
-                            <a class="Leaflet__post-title" href="<?php the_permalink();?>"><?php the_title();?></a>
-                        </h3>
-                        <p class="Leaflet__author-date"><?php echo get_the_date(); ?> by <?php echo get_field('author');?></p>
-                        <?php 
-                            $image = get_field('image');
-                            $size = 'large'; // (thumbnail, medium, large, full or custom size)
-                            if( $image ) {
-                                $imageSrc = wp_get_attachment_image_src( $image, $size )[0];
-                            }
-                        ?>
-                        <img
-                            class="Leaflet__img"
-                            src="<?php echo $imageSrc ?>"
-                            alt=""
-                        />
-                        <hr>
-                    </div>
-                <?php
-                }
-            }
-        ?>
-        <?php the_posts_pagination( array(
-            'mid_size'  => 2,
-            'prev_text' => __( 'NEWER POSTS', 'textdomain' ),
-            'next_text' => __( 'OLDER POSTS', 'textdomain' ),
-        ) ); ?>
+<?php get_header() ?>
+<?php $postCount = 0 ?>
+<div class="Blog">
+  <div class="Blog__home">
+    <h1 class="Blog__title">THE LEAFLET</h1>
+    <div class="Blog__posts-list">
+      <?php
+if (have_posts())
+{
+    while (have_posts())
+    {
+        the_post();
+        $postCount++; ?>
+      <div class="Blog__post">
+        <h3>
+          <a class="Blog__post-title" href="<?php the_permalink(); ?>"
+            ><?php the_title(); ?></a
+          >
+        </h3>
+        <p class="Blog__author-date">
+          <?php echo get_the_date(); ?>
+          by
+          <?php echo get_field('author'); ?>
+        </p>
+        <?php
+        $image = get_field('image');
+        $size = 'large'; // (thumbnail, medium, large, full or custom size)
+        if ($image)
+        {
+            $imageSrc = wp_get_attachment_image_src($image, $size) [0];
+        }
+?>
+        <img class="Blog__img" src="<?php echo $imageSrc ?>" alt="" />
+        <hr />
+      </div>
+      <?php
+    }
+}
+?>
+      <?php the_posts_pagination(array(
+    'mid_size' => 2,
+    'prev_text' => __('NEWER POSTS', 'textdomain') ,
+    'next_text' => __('OLDER POSTS', 'textdomain') ,
+)); ?>
     </div>
+  </div>
 </div>
 <?php get_footer(); ?>
--- single-resources.php
+++ single-resources.php
@@ -1,1 +1,31 @@
-hello
+<?php get_header(); ?>
+<div class="Leaflet">
+    <p></p>
+  <div class="Leaflet__loop">
+    <?php 
+        if( have_posts() ) {
+            while( have_posts() ) {
+                the_post();
+                ?>
+    <?php 
+                    $image = get_field('image');
+                    $size = 'full'; // (thumbnail, medium, large, full or custom size)
+                    if( $image ) {
+                        echo wp_get_attachment_image( $image, $size );
+                    }
+                    ?>
+    <p class="Leaflet__loop-meta">
+      <?php echo get_the_date(); ?>
+    </p>
+    <h2 class="Leaflet__loop-title"><?php the_title();?></h2>
+    <div class="Leaflet__loop-content">
+      <?php the_content();?>
+    </div>
+    <?php echo get_the_category_list(); ?>
+    <?php
+            }
+        }
+    ?>
+  </div>
+</div>
+<?php get_footer(); ?>
