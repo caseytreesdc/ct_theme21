84be0130df4df23459221dd9a3e9fa3fa9adad1d
TKhosla <tkhosla@caseytrees.org>
2021-03-01 11:24:30 -0500

    feature a Leaflet Post, edit

--- home.php
+++ home.php
@@ -3,6 +3,7 @@
 <section class="Blog__section-title">
   <h1 class="Blog__title-padded">THE LEAFLET</h1>
 </section>
+
 <?php get_template_part('template-parts/query', 'Feature') ?>
 <?php get_template_part('template-parts/query', 'Recents')?>
 <?php get_template_part('template-parts/query', 'Stories')?>
