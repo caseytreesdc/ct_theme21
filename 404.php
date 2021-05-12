34333b7b23e60badabcb1240445d1ee65e480fe4
TKhosla <tkhosla@caseytrees.org>
2021-05-11 21:56:46 -0400

    updated 404 template

--- 404.php
+++ 404.php
@@ -1,1 +1,22 @@
-<h1>404</h1>
+<?php get_header(); ?>
+<div
+  class="Page__masthead tall-masthead"
+  style="
+    background: linear-gradient(
+      rgba(44, 16, 16, 0),
+      rgba(44, 16, 16, 0),
+      rgba(44, 16, 16, 0),
+      rgba(44, 16, 16, .4),
+      rgba(44, 16, 16, 1)),
+      url(https://caseytrees.org/wp-content/uploads/2017/05/shutterstock_88966654-1920x627-c-default.jpg);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  "
+>
+  <div class="Page__heading-text">
+    <h1 class="Page__title">404 - Hang in there!</h1>
+      <p class="Page__subtitle">This page doesn't seem to exist! Please navigate to a different page.</p>
+  </div>
+</div>
+<?php get_footer(); ?>
